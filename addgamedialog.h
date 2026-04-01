2dbf05f578ac5549453c648f1bc2cfbfc23637c5
Zumich <vladyslavstratiichuk@gmail.com>
2026-04-01 14:06:03 +0300

    feat(dialog): описано інтерфейс діалогу додавання гри

---
+++ addgamedialog.h
@@ -0,0 +1,44 @@
+#ifndef ADDGAMEDIALOG_H
+#define ADDGAMEDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class AddGameDialog;
+}
+
+class AddGameDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    // Конструктор (створення вікна)
+    explicit AddGameDialog(QWidget *parent = nullptr);
+
+    // Деструктор (очищення пам'яті)
+    ~AddGameDialog();
+
+    // Отримати назву гри
+    QString getTitle() const;
+
+    // Отримати жанр гри
+    QString getGenre() const;
+
+    // Отримати платформу
+    QString getPlatform() const;
+
+    // Отримати кількість годин
+    int getHours() const;
+
+    // Отримати загальну кількість досягнень
+    int getTotalAchievements() const;
+
+    // Отримати відкриті досягнення
+    int getOpenedAchievements() const;
+
+private:
+    // Вказівник на UI форму (елементи з Designer)
+    Ui::AddGameDialog *ui;
+};
+
+#endif
